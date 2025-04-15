d976ea64c96813ddf12b19f5560d6fdd9021f695
nitishcodes25 <nitish.codes25@gmail.com>
2025-04-15 14:53:53 +0530

    feat: add basic editor tools like heading, image, drag&drop and table etc

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,9 +1,66 @@
-const Editor = () => {
-    return (
-        <div>
-            Editor Page
-        </div>
-    )
-}
+"use client";
 
-export default Editor
+import { useEditor, EditorContent } from "@tiptap/react";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Image from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image';
+import StarterKit from "@tiptap/starter-kit";
+
+const Editor = () => {
+  const editor = useEditor({
+    editorProps: {
+      attributes: {
+        style: "padding-left: 56px; padding-right:56px",
+        class:
+          "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px]  w-[816px] pt-10 pr-14 pb-10",
+      },
+    },
+    extensions: [
+      StarterKit,
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+      Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
+      Image,
+      ImageResize
+    ],
+    content: `
+      <p>This is a basic example of implementing images. Drag to re-order.</p>
+        <img src="https://placehold.co/800x400" />
+        <img src="https://placehold.co/800x400/6A00F5/white" />
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>`,
+  });
+  return (
+    <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <div className="min-w-max flex justify-center w-[816px] py-4 mx-auto print:py-0 print:w-full print:min-w-0">
+        <EditorContent editor={editor} />
+      </div>
+    </div>
+  );
+};
+
+export default Editor;
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -6,8 +6,7 @@ interface DocumentIdPageProps {
 const DocumentIdPage = async({params}: DocumentIdPageProps) => {
     const {documentId} = await params;
     return (
-        <div>
-            <h1>Document id: {documentId}</h1>
+        <div className="min-h-screen bg-[#FAFBFD]">
             <Editor/>
         </div>
     )
